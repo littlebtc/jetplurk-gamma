f948b5dcce501825bf24b45cb29aa53c15a65807
Hsiao-Ting Yu <sst.dreams@gmail.com>
2010-08-23 22:00:44 +0800

    Prepare migration: move all JS/HTML to data/ ; remove json2.js as not needed for Fx 3.6/4

--- JetPlurk_slideframe.js
+++
@@ -1,7 +0,0 @@
-jetpack.future.import('slideBar');
-jetpack.slideBar.append({
-	icon: "http://www.plurk.com/favicon.ico",
-	width: 300,
-	persist: true,
-	url: "file:///＊請修改指向本地目錄＊/Coscup-lighting/index.html"
-});
